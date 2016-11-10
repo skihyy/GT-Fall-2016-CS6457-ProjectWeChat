2cce3e109309da204c7ed386e02487acab148ebe
Villian <yuyang.he@gatech.edu>
2016-11-09 22:36:59 -0500

    PauseMenu
    New pause menu added.

---
+++ Assets/Scripts/PauseMenu/PauseMenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuController : MonoBehaviour
+{
+	public GameObject cameraObject;
+	public GameObject backgroundShader;
+	public GameObject pauseMenuCanvas;
+
+	private bool isCurrentPaused = false;
+
+	public void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			if (isCurrentPaused)
+			{				
+				pauseMenuCanvas.SetActive (false);
+				backgroundShader.SetActive (false);
+				Time.timeScale = 1;
+			}
+			else
+			{				
+				pauseMenuCanvas.SetActive (true);
+				backgroundShader.SetActive (true);
+
+				backgroundShader.transform.position = cameraObject.transform.position;
+
+				Time.timeScale = 0;
+			}
+
+			isCurrentPaused = !isCurrentPaused;
+		}
+	}
+}
